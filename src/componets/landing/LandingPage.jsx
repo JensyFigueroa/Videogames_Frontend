2cff7e993a74ce2bc1e9d17a88464db4f733740b
Jensy Figueroa <jensy1418@gmail.com>
2023-05-02 19:27:10 -0400

    seventh deploy

--- src/componets/landing/LandingPage.jsx
+++ src/componets/landing/LandingPage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './LandingPage.module.css'
 
-export default function LandingPage () {
+export default function LandingPage() {
   return (
     <div className={styles.boxLanding}>
-        <div className={styles.main}>
+      <div className={styles.main}>
+        <div>
+
           <h1>Welcome to the video games app</h1>
           <h4>Where you can create your Cards with your favorite video game</h4>
           <Link to='/videogames' className={styles.btn}>Start now</Link>
         </div>
       </div>
+    </div>
   )
 }
