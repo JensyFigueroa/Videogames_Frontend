a1c4a9f297cbe0a30118afccee97ceb7faee4c8e
Jensy Figueroa <jensy1418@gmail.com>
2023-05-02 19:30:31 -0400

    first deploy

--- src/componets/landing/LandingPage.jsx
+++ src/componets/landing/LandingPage.jsx
@@ -7,7 +7,6 @@ export default function LandingPage() {
     <div className={styles.boxLanding}>
       <div className={styles.main}>
         <div>
-
           <h1>Welcome to the video games app</h1>
           <h4>Where you can create your Cards with your favorite video game</h4>
           <Link to='/videogames' className={styles.btn}>Start now</Link>
