409451a25eeb647dda59f2d1d0b0d870027401f6
Jensy Figueroa <jensy1418@gmail.com>
2023-05-04 20:58:22 -0400

    first deploy

--- src/componets/VideoGames/VideoGames.jsx
+++ src/componets/VideoGames/VideoGames.jsx
@@ -32,12 +32,6 @@ export default function VideoGames() {
         {/* <div className={styles.wave}></div> */}
       </div>
 
-      <div className={styles.wave}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="rgba(2, 4, 74)" d="M0,96L80,128C160,160,320,224,480,240C640,256,800,224,960,208C1120,192,1280,192,1360,192L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-        </svg>
-      </div>
-
       <div className={styles.containerSearch}>
         <SearchBar onSearch={onSearch} />
 
